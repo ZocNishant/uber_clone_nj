b32eb49987ec1df9ef9c403bd35f6c5321fbcabc
Nishant Zoc <zocnishant@gmail.com>
2024-12-06 16:53:17 +0545

    updated backend

--- Backend_uber/services/user.service.js
+++ Backend_uber/services/user.service.js
@@ -6,4 +6,14 @@ module.exports.createUser = async({
     if(!firstname || email || password) {
         throw new Error('All fields are required.')
     }
+    const user = userModel.create({
+        fullname: {
+            firstname,
+            lastname,
+        },
+        email,
+        password
+    })
+
+    return user
 }
